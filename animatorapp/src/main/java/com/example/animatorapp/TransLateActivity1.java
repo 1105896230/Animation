564af7c86f797d5ab66ab706c798121c03a8625b
fmt <1105896230@qq.com>
2016-09-02 17:20:44 +0800

    加入界面

--- animatorapp/src/main/java/com/example/animatorapp/TransLateActivity1.java
+++ animatorapp/src/main/java/com/example/animatorapp/TransLateActivity1.java
@@ -1,9 +1,15 @@
 package com.example.animatorapp;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.animatorapp.databinding.ActivityTranslateBinding;
+
+import static com.example.animatorapp.BaseActivity.EXTRA_SAMPLE;
+
 public class TransLateActivity1 extends AppCompatActivity {
+    private Sample sample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +17,9 @@ public class TransLateActivity1 extends AppCompatActivity {
         setContentView(R.layout.activity_translate);
     }
 
+    private void bindData() {
+        ActivityTranslateBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_translate);
+        sample = (Sample) getIntent().getExtras().getSerializable(EXTRA_SAMPLE);
+        binding.setTransition1Sample(sample);
+    }
 }
